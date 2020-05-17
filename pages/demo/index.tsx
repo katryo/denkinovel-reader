beacd02b74ad5f00634bb378e9a5becc46d1bf18
katryo <katoryo55@gmail.com>
2020-05-17 14:39:23 -0400

    Implement two-layer fade

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -1,29 +1,77 @@
-import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import { CSSTransition } from 'react-transition-group'
 import { useState } from 'react'
 
-const BackgroundContainer = () => {
-  const [colors, setColor] = useState(['blue'])
+const WHITE = '#ffffff'
+const PINK = '#fecbc8'
+const BLUE = '#dceff5'
 
-  const items = colors.map((item, i) => (
-    <div key={item}>
-      {item} {i}
-    </div>
-  ))
+const BackgroundContainer = () => {
+  const [lowerBGColor, setLowerBGColor] = useState(WHITE)
+  const [upperBGColor, setUpperBGColor] = useState(WHITE)
+
+  const [upperBGIn, setUpperBGIn] = useState(true)
+
+  const sleep = async (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
   return (
     <div>
-      <button
-        onClick={() => {
-          const conc = colors.concat(colors.length.toString())
-          setColor(conc)
-        }}>
-        btn
-      </button>
-      background
-      <ReactCSSTransitionGroup transitionName='example' transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-        {items}
-      </ReactCSSTransitionGroup>
-      <div>{items.length}</div>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          left: 0,
+          top: 0,
+          background: lowerBGColor,
+          position: 'fixed',
+        }}></div>
+      <CSSTransition in={upperBGIn} timeout={2000} classNames='upper-bg'>
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            background: upperBGColor,
+            position: 'fixed',
+          }}></div>
+      </CSSTransition>
+      <div style={{ position: 'relative' }}>
+        <button
+          onClick={async () => {
+            setLowerBGColor(PINK)
+            setUpperBGIn(false)
+            await sleep(2000)
+            setUpperBGColor(PINK)
+            setUpperBGIn(true)
+          }}>
+          pink
+        </button>
+        <button
+          onClick={async () => {
+            setLowerBGColor(BLUE)
+            setUpperBGIn(false)
+            await sleep(2000)
+            setUpperBGColor(BLUE)
+            setUpperBGIn(true)
+          }}>
+          blue
+        </button>
+        <button
+          onClick={() => {
+            setUpperBGIn(false)
+          }}>
+          setUpperBGIn(false);
+        </button>
+        <p>本文</p>
+        <p>本文</p>
+        <p>本文</p>
+        <p>本文</p>
+        <p>本文</p>
+        <p>本文</p>
+        <p>本文</p>
+      </div>
     </div>
   )
 }
@@ -31,7 +79,6 @@ const BackgroundContainer = () => {
 const Demo = () => {
   return (
     <div>
-      demo
       <BackgroundContainer />
     </div>
   )
