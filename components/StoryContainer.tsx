4d1a3654d54bea2af0f4ea02b42f612fd346c77a
katryo <katoryo55@gmail.com>
2020-06-08 10:30:04 -0400

    Enable Google fonts

--- components/StoryContainer.tsx
+++ components/StoryContainer.tsx
@@ -371,7 +371,7 @@ const StoryContainer = (props: { episode: Episode }) => {
             fontSize: 17.5,
             lineHeight: 1.8,
             fontFamily:
-              "'游明朝',YuMincho,'ヒラギノ明朝 Pr6N','Hiragino Mincho Pr6N','ヒラギノ明朝 ProN','Hiragino Mincho ProN','ヒラギノ明朝 StdN','Hiragino Mincho StdN',HiraMinProN-W3,'HGS明朝B','HG明朝B',dcsymbols,'Helvetica Neue',Helvetica,Arial,'ヒラギノ角ゴ Pr6N','Hiragino Kaku Gothic Pr6N','ヒラギノ角ゴ ProN','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ StdN','Hiragino Kaku Gothic StdN','Segoe UI',Verdana,'メイリオ',Meiryo,sans-serif",
+              "'游明朝',YuMincho,'ヒラギノ明朝 Pr6N','Hiragino Mincho Pr6N','ヒラギノ明朝 ProN','Hiragino Mincho ProN','ヒラギノ明朝 StdN','Hiragino Mincho StdN',HiraMinProN-W3,'Noto Serif JP','HGS明朝B','HG明朝B',dcsymbols,'Helvetica Neue',Helvetica,Arial,'ヒラギノ角ゴ Pr6N','Hiragino Kaku Gothic Pr6N','ヒラギノ角ゴ ProN','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ StdN','Hiragino Kaku Gothic StdN','Segoe UI',serif",
           }}
         >
           <PageList
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,28 @@
-import Head from 'next/head'
-import { Flex } from '@chakra-ui/core'
+import Head from 'next/head';
+import { Flex } from '@chakra-ui/core';
 
 const Home = () => (
-  <div className='container'>
+  <div className="container">
     <Head>
       <title>Denkinovel - Novels like movies</title>
-      <link rel='icon' href='/favicon.ico' />
+      <link rel="icon" href="/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet"></link>
     </Head>
 
     <main>
-      <Flex flexDirection='column'>
+      <Flex flexDirection="column">
         <Flex>
           <h1>Denkinovel</h1>
         </Flex>
 
         <Flex>
-          <p className='description'>
+          <p className="description">
             Get started by editing <code>pages/index.js</code>
           </p>
         </Flex>
       </Flex>
     </main>
   </div>
-)
+);
 
-export default Home
+export default Home;
