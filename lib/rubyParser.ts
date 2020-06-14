8361b61e644fb1d01bb4d0c98fde113cd0daf360
katryo <katoryo55@gmail.com>
2020-06-14 12:33:08 -0400

    Implement rubyParser

--- lib/rubyParser.ts
+++ lib/rubyParser.ts
@@ -1,6 +1,8 @@
 const PLAIN = 'PLAIN';
 const RUBY = 'RUBY';
 
+const DEFAULT_IDX = -1;
+
 interface PlainElement {
   type: typeof PLAIN;
   plainText: string;
@@ -14,14 +16,163 @@ interface RubyElement {
 
 type TextElement = PlainElement | RubyElement;
 
+interface Props {
+  elements: TextElement[];
+  baseStartIdx: number;
+  rubyStartIdx: number;
+  lastRubyEndIdx: number;
+}
+
+type Action = (char: string, cur: Props, i: number, text: string) => Props;
+
+const errorAction = (char: string, cur: Props, i: number, text: string) => {
+  const spaces = [];
+  for (let index = 0; index < i; index++) {
+    spaces.push(' '); // TODO: Handle multi byte characters
+  }
+  const spaceString = spaces.join('');
+  throw new Error(`Parse error. Text index: ${i}.\n${text}\n${spaceString}^`);
+};
+
+const noOpAction: Action = (char: string, cur: Props, i: number, text: string) => {
+  return cur;
+};
+
+const baseStartAction: Action = (char: string, cur: Props, i: number, text: string) => {
+  if (cur.baseStartIdx !== DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  if (cur.rubyStartIdx !== DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  cur.baseStartIdx = i;
+  return cur;
+};
+
+const rubyStartAction: Action = (char: string, cur: Props, i: number, text: string) => {
+  if (cur.baseStartIdx === DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  if (cur.rubyStartIdx !== DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  cur.rubyStartIdx = i;
+  return cur;
+};
+
+const rubyEndAction: Action = (char: string, cur: Props, i: number, text: string) => {
+  if (cur.baseStartIdx === DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  if (cur.rubyStartIdx === DEFAULT_IDX) {
+    errorAction(char, cur, i, text);
+  }
+  const elem: RubyElement = {
+    type: RUBY,
+    baseText: text.slice(cur.baseStartIdx + 1, cur.rubyStartIdx),
+    rubyText: text.slice(cur.rubyStartIdx + 1, i),
+  };
+  cur.rubyStartIdx = DEFAULT_IDX;
+  cur.baseStartIdx = DEFAULT_IDX;
+  cur.lastRubyEndIdx = i;
+  cur.elements.push(elem);
+  return cur;
+};
+
 const isPlain = (elem: TextElement): elem is PlainElement => {
   return elem.type === PLAIN;
 };
 
+const DUMMY_STATE = 'DUMMY_STATE';
+
+interface ActionAndNextState {
+  action: Action;
+  nextState: State | 'DUMMY_STATE';
+}
+
+interface State {
+  '|': ActionAndNextState;
+  '｜': ActionAndNextState;
+  '《': ActionAndNextState;
+  '》': ActionAndNextState;
+  OTHERS: ActionAndNextState;
+}
+
+const INIT_PROPS: Props = {
+  elements: [],
+  baseStartIdx: DEFAULT_IDX,
+  rubyStartIdx: DEFAULT_IDX,
+  lastRubyEndIdx: DEFAULT_IDX,
+};
+
+const initState: State = {
+  '|': { action: baseStartAction, nextState: DUMMY_STATE },
+  '｜': { action: baseStartAction, nextState: DUMMY_STATE },
+  '《': { action: noOpAction, nextState: DUMMY_STATE },
+  '》': { action: noOpAction, nextState: DUMMY_STATE },
+  OTHERS: { action: noOpAction, nextState: DUMMY_STATE },
+};
+initState['《'].nextState = initState;
+initState['》'].nextState = initState;
+initState.OTHERS.nextState = initState;
+
+const inRubyState: State = {
+  '|': { action: noOpAction, nextState: DUMMY_STATE },
+  '｜': { action: noOpAction, nextState: DUMMY_STATE },
+  '《': { action: noOpAction, nextState: DUMMY_STATE },
+  '》': { action: rubyEndAction, nextState: initState },
+  OTHERS: { action: noOpAction, nextState: DUMMY_STATE },
+};
+inRubyState['｜'].nextState = inRubyState;
+inRubyState['|'].nextState = inRubyState;
+inRubyState['《'].nextState = inRubyState;
+inRubyState.OTHERS.nextState = inRubyState;
+
+const inBaseState: State = {
+  '|': { action: noOpAction, nextState: DUMMY_STATE },
+  '｜': { action: noOpAction, nextState: DUMMY_STATE },
+  '《': { action: rubyStartAction, nextState: inRubyState },
+  '》': { action: noOpAction, nextState: DUMMY_STATE },
+  OTHERS: { action: noOpAction, nextState: DUMMY_STATE },
+};
+inBaseState['｜'].nextState = inBaseState;
+inBaseState['|'].nextState = inBaseState;
+inBaseState['》'].nextState = inBaseState;
+inBaseState.OTHERS.nextState = inBaseState;
+
+initState['｜'].nextState = inBaseState;
+initState['|'].nextState = inBaseState;
+
 const parse = (text: string): TextElement[] => {
-  const ret: TextElement[] = [];
-  ret.push({ type: PLAIN, plainText: text });
-  return ret;
+  let props = JSON.parse(JSON.stringify(INIT_PROPS));
+  let state = initState;
+
+  const step = (char: string, i: number) => {
+    let next = char;
+    if (!(char in state)) {
+      next = 'OTHERS';
+    }
+    const { action, nextState } = state[next];
+    props = action(char, props, i, text); // Passing whole the text is memory consuming?
+    if (nextState === DUMMY_STATE) {
+      if (i < text.length - 1) {
+        throw new Error('DUMMY_STATE.');
+      }
+    } else {
+      state = nextState;
+    }
+  };
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    step(char, i);
+  }
+
+  if (props.lastRubyEndIdx !== text.length - 1) {
+    props.elements.push({ type: PLAIN, plainText: text.slice(props.lastRubyEndIdx + 1) });
+  }
+
+  return props.elements;
 };
 
 export { isPlain, parse };
