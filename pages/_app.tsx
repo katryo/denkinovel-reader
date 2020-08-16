6f3cba8d106c32537f4bd627358bb730aad14d7e
katryo <katoryo55@gmail.com>
2020-08-16 14:39:26 -0700

    Install why-did-you-render

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles.css'
+import '../styles.css';
+import React from 'react';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
